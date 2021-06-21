2643afe81b725e9a734fc9a313c05155670b6709
joshsrai <joshsrai@gmail.com>
2021-06-21 17:37:25 -0400

    fetched data

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 function App() {
+
+    const [quotes, setQuotes] = React.useState([])
+    const [randomQuote, setRandomQuote] = React.useState([])
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const response = await fetch ("https://type.fit/api/quotes")
+            const data = await response.json();
+
+            setQuotes(data);
+            let randIndex = Math.floor(Math.random() * data.length)
+            setRandomQuote(data[randIndex])
+        }
+        fetchData()
+    }, [])
+
     return (
-        <div>hello </div>
+        <div>hello 
+        
+        {quotes.map(quote => (
+            <div>{quote.text}</div>
+        ))}
+        </div>
     );
 }
 
