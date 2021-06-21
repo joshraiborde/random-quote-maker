5b510b9eb6efeaf31a596df030a1acdf768ec9e8
joshsrai <joshsrai@gmail.com>
2021-06-21 18:07:14 -0400

    added cards, authors and texts, button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function App() {
 
     const [quotes, setQuotes] = React.useState([])
-    const [randomQuote, setRandomQuote] = React.useState([])
+    const [randomQuote, setRandomQuote] = React.useState("")
 
     React.useEffect(() => {
         async function fetchData() {
@@ -16,12 +16,32 @@ function App() {
     }, [])
 
     return (
-        <div>hello 
-        
-        {quotes.map(quote => (
-            <div>{quote.text}</div>
-        ))}
+        <div className="contianer pt-5">
+            <div className="jumbotron">
+                <div className="card">
+                    <div className="card-header">Inspiriational Quotes:</div>
+                    <div className="card-body">
+                    {randomQuote ? (
+                        <>
+                    <h5 className="card-title">- {randomQuote.author || "No Author" }</h5>
+                    <p className="card-text"> &quot;{randomQuote.text}&quot;</p>
+                        </>
+                    ) : (
+                        <h2>Loading</h2>
+                    )}
+                    <div className="row">
+                    <button onClick={getNewQuote}>New Quote</button>
+                        <a href=""></a>
+                        <a href=""></a>
+
+                    </div>
+
+                    
         </div>
+        </div>
+        </div>
+        </div>
+
     );
 }
 
