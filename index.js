2fbae16e803fcf6899c9d32750d6273379e5ef47
joshsrai <joshsrai@gmail.com>
2021-06-22 18:39:18 -0400

    deleted extra lines and such

--- index.js
+++ index.js
@@ -31,47 +31,44 @@ function App() {
 
     return (
         <div style={{backgroundColor: color, minHeight: "100vh"}}>
-        <div className="contianer pt-5">
-            <div className="jumbotron">
-                <div className="card">
-                    <div className="card-header">Quotable Quotes:</div>
-                    <div className="card-body">
-                    {randomQuote ? (
-                        <>
-                    <h5 className="card-title">- {randomQuote.author || "No Author" }</h5>
-                    <p className="card-text"> &quot;{randomQuote.text}&quot;</p>
-                        </>
-                    ) : (
-                        <h2>Loading...</h2>
-                    )}
-                    <div className="row">
-                    <button onClick={getNewQuote} className="btn btn-primary">New Quote</button>
-                        <a href={
-                            "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-                            encodeURIComponent(
-                                '"' + randomQuote.text + '"' + randomQuote.author
-                            )
-                            } target="_blank" className="btn btn-warning">
-                            <i className="fa fa-twitter"></i>
-                        </a>
-                        <a href={
-                            "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" + 
-                            encodeURIComponent(randomQuote.author) +
-                            "&content=" +
-                            encodeURIComponent(randomQuote.text) +
-                            "&canonicalUrl=https%3A%2F%2fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
-                        } target="_blank" className="btn btn-danger">
-                        <i className="fa fa-tumblr"></i>
-                        </a>
-
-                    </div>
-
-                    
+            <div className="contianer pt-5">
+                <div className="jumbotron">
+                    <div className="card">
+                        <div className="card-header">Quotable Quotes:</div>
+                            <div className="card-body">
+                                {randomQuote ? (
+                                <>
+                                <h5 className="card-title">- {randomQuote.author || "No Author" }</h5>
+                                    <p className="card-text"> &quot;{randomQuote.text}&quot;</p>
+                                </>
+                                ) : (
+                                <h2>Loading...</h2>
+                                )}
+                                    <div className="row">
+                                        <button onClick={getNewQuote} className="btn btn-primary">New Quote</button>
+                                        <a href={
+                                            "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+                                            encodeURIComponent(
+                                                '"' + randomQuote.text + '"' + randomQuote.author
+                                            )
+                                            } target="_blank" className="btn btn-warning">
+                                            <i className="fa fa-twitter"></i>
+                                        </a>
+                                        <a href={
+                                            "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" + 
+                                            encodeURIComponent(randomQuote.author) +
+                                            "&content=" +
+                                            encodeURIComponent(randomQuote.text) +
+                                            "&canonicalUrl=https%3A%2F%2fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+                                            } target="_blank" className="btn btn-danger">
+                                        <i className="fa fa-tumblr"></i>
+                                        </a>
+                                     </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-        </div>
-</div>
     );
 }
 
